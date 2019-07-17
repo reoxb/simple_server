ba17a15967e2079008867d913afbb61e1b7b27d8
SUAREZ SOSA <J.S.SOSA@CIMMYT.ORG>
2019-07-16 23:03:07 -0500

    Its working now

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const parsedUrl = require('url');
-// const sg_api = require('./sg_api');
 const sg_api_get = require('./sg_api_get');
 const sg_api_post = require('./sg_api_post');
 
@@ -10,8 +9,8 @@ const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 8080;
-// ethernet
 const hostname = 'localhost';
+// ethernet
 // const hostname = '172.17.56.134';
 // ethernet sala juntas
 // const hostname = '172.17.56.131';
@@ -24,22 +23,20 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.post('/action_post', (req, res) => {
-
   // const url = parsedUrl.parse(req.url, true);
   // console.log(req.body)
   // delete req.body.insertButton
 
-  const elbody =
-  { 
-    'text#experiment_name': 'uds',
-    'text#comments': 'sd',
+  const elbody = { 
+    'text#experiment_name': "EXPT e78ef34d8d498ad46b2625aa91fe2e03",
+    'text#comments': "It's Working Now!",
     'number#user_id': '1',
-    'date#creation_timestamp': '07/16/2019',
+    'date#creation_timestamp': "06/16/2019",
     'number#service_id': '1',
     'number#sample_batch_id': '1',
-    'number#crop_id': '1',
-    'text#ticket_number': 'ufhiu29832fowhio',
-    'number#request_status_id': '1',
+    'number#crop_id': '3',
+    'text#ticket_number': "TICKET 1cc3f06a97c2e7c17cef224c42daaf5c",
+    'number#request_status_id': '2',
     'number#subservice_id': '1' 
   }
 
@@ -75,27 +72,8 @@ app.post('/action_post', (req, res) => {
     }
   }
 
-  // const otherJsonBody = {
-  //   "_postrequests": {
-  //       "experiment_name": "EXPT e78ef34d8d498ad46b2625aa91fe2e03",
-  //       "comments": "test_component_1",
-  //       "user_id": 1,
-  //       "creation_timestamp": "2019-06-14+00:00",
-  //       "service_id": 1,
-  //       "sample_batch_id": 1,
-  //       "crop_id": 3,
-  //       "ticket_number": "TICKET 1cc3f06a97c2e7c17cef224c42daaf5c",
-  //       "request_status_id": 2,
-  //       "subservice_id": 1
-  //   }
-  // }
-  // console.log(otherJsonBody)
-  // console.log(JSON.stringify(otherJsonBody))
-
-  console.log(jsonBody)
-
   // const entity = url.query.entity;
-  sg_api_post( (data => console.log(data)), JSON.stringify(jsonBody ) )
+  sg_api_post( (data => console.log(data)), jsonBody)
   res.send('Send ok')
 });
 
--- sg_api_post.js
+++ sg_api_post.js
@@ -1,34 +1,5 @@
 const https = require('http')
 
-// const postdata = json.stringify({
-//     "_postrequests": {
-//         "experiment_name": "expt e78ef34d8d498ad46b2625aa91fe2e03",
-//         "comments": "test_component_1",
-//         "user_id": 1,
-//         "creation_timestamp": "2019-06-14+00:00",
-//         "service_id": 1,
-//         "sample_batch_id": 1,
-//         "crop_id": 3,
-//         "ticket_number": "ticket 1cc3f06a97c2e7c17cef224c42daaf5c",
-//         "request_status_id": 2,
-//         "subservice_id": 1
-//     }
-// });
-
-// const options = {
-//     protocol: 'http:',
-//     host: '172.17.60.49',
-//     port: 8280,
-//     path: '/services/samplemanagerdm/requests',
-//     method: 'post',
-//     headers: {
-//         'content-type': 'application/json',
-//         'content-length': postdata.length
-//         // 'content-type': 'application/x-www-form-urlencoded',
-//         // 'content-length': buffer.bytelength(postdata)
-//     }
-// }
-
 module.exports = function (callback, jsonBody) {
 
 const postdata = JSON.stringify(jsonBody);
@@ -37,8 +8,8 @@ const options = {
     protocol: 'http:',
     host: '172.17.60.49',
     port: 8280,
-    path: '/services/samplemanagerdm/requests',
-    method: 'post',
+    path: '/services/SampleManagerDM/requests',
+    method: 'POST',
     headers: {
         'content-type': 'application/json',
         'content-length': postdata.length
@@ -56,7 +27,6 @@ const options = {
 
         res.on('end', () => {
             try {
-                // let parsedData = JSON.parse(rawData);
                 // callback(parsedData);
                 callback(rawData);
             } catch (e) {
